636e6c005957969284910340166d59d156470682
henry lee <djteski@gmail.com>
2020-03-30 01:53:14 -0500

    About Pokemon added

---
+++ aboutpokemon.js
@@ -0,0 +1,35 @@
+// document.addEventListener(click, function aboutPokemon (buttonEvent) {
+//     return buttonEvent
+// })
+
+
+// function buttonEvent () {
+//     const buttonElement = document.getElementsByClassName("btn-warning")
+//     buttonElement.innerHTML = `
+//     <div class="aboutPokemon">Hello</div>
+//     `
+//     console.log("about pokemon inserted!")
+// }
+
+
+// document.addEventListener("click", function clickEvent (evt) {
+    
+//     const btnEl = document.getElementById("aboutButton")
+//     const newEl = document.createElement("div")
+
+
+//     newEl.innerHTML = `
+//     <div class="about">Pokemon is a...</div>
+//     `
+//     console.log("returning")
+//     return 
+// })
+
+window.onload = function () {
+
+    what ();
+
+    function what () {
+document.getElementById("aboutContent").innerHTML = "Finally!!";
+    };
+}
