4425f706ec9453a5325260cfaad0e4a1fa239452
henry lee <djteski@gmail.com>
2020-03-30 23:25:42 -0500

    Still working on About Pokemon

--- aboutpokemon.js
+++ aboutpokemon.js
@@ -1,11 +1,6 @@
-window.onload = function () {
 
-    what ();
-
-    function what () {
 document.getElementById("aboutContent").innerHTML = `<div class="wikiPokemon">Pokemon, also known as Pocket Monsters in Japan, is a Japanese media franchise managed by the Pokemon Company, a company founded and with shares divided between Nintendo, Game Freak, and Creatures. The franchise copyright and Japanese trademark is shared by all three companies, but Nintendo is the sole owner of the trademark in other countries. The franchise was created by Satoshi Tajiri in 1995, and is centered on fictional creatures called "Pokemon", which humans, known as Pokemon Trainers, catch and train to battle each other for sport. The English slogan for the franchise is "Gotta Catch 'Em All". Works within the franchise are set in the Pokemon universe.</div>`;
-    };
-}
+
 
 $(document).ready(function () {
     $("button").click(function clickEvent () {
