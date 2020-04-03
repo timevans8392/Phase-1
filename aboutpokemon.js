536088d228b1b3f82cd1ba53a34dd450bb67d9d3
henry lee <djteski@gmail.com>
2020-04-02 21:48:32 -0500

    aboutpokemon javascript updated

--- aboutpokemon.js
+++ aboutpokemon.js
@@ -41,18 +41,18 @@
 //         })
 //     })
 // })
-
+var number = 1
 $(document).ready(function () {
-    $('button').click(function clickEvt (Evt) {
+    $('#firstButton').click(function clickEvt (Evt) {
         console.log("Button has clicked!")
-        var number = 1
-        number++
+        $('.rowClass').html(number++)
+        
   $.getJSON(`https://pokeapi.co/api/v2/pokemon/${
             Math.floor(Math.random()*963+1)
         }`, function clickResult (result) {
             console.log(result)
             const image = result.sprites.front_default
-            $('.rowClass').html(`<p>1</p>`)
+
             $('.image').html(`<img src="${image}">`)
             $('.name').html(`<td>${result.name}</td>`)
             $('.height').html(`<td>${result.height} ft </td>`)
@@ -65,14 +65,3 @@ $(document).ready(function () {
     document.getElementById("aboutContent").innerHTML = `<div class="wikiPokemon">Pokemon, also known as Pocket Monsters in Japan, is a Japanese media franchise managed by the Pokemon Company, a company founded and with shares divided between Nintendo, Game Freak, and Creatures. The franchise copyright and Japanese trademark is shared by all three companies, but Nintendo is the sole owner of the trademark in other countries. The franchise was created by Satoshi Tajiri in 1995, and is centered on fictional creatures called "Pokemon", which humans, known as Pokemon Trainers, catch and train to battle each other for sport. The English slogan for the franchise is "Gotta Catch 'Em All". Works within the franchise are set in the Pokemon universe.</div>`;
 }
 )
-
-
-function numberPokemon () {
-    var number = 0
-
-    // for (let i = 1; i <= 964; i++) {
-        console.log(number++) 
-    // }
-}
-
-$('#firstButton').click(numberPokemon)
